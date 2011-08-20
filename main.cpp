7fabf165af1a2f0aae4e08ada8a6daf214282225
Shashank Kumar <shashankkumar.me@gmail.com>
2011-08-20 19:22:40 +0530

    added following: 1. code to change directory to the judge's 2. improved logging code 3. Added RUNONCE support 4. Added variable CurrentIteration

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /**************************************************************************
 CodeRunner - the online judge
 Author: Shashank Kumar <shashankkumar.me@gmail.com>
-Copyright (c): 2010 All rights reserved
+Copyright (c): 2011 All rights reserved
 Version: 1.5
 
  * This is a free software; you can redistribute it and/or
@@ -9,7 +9,7 @@ Version: 1.5
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
@@ -20,76 +20,56 @@ shashankkumar.me@gmail.com
 ****************************************************************************/
 #include "includes.h"
 #include "ContentParser.h"
-#include "CurlWrapper.h"
 #include "FileHandle.h"
 #include "Logs.h"
-s
+
 int main()
 {
-
-    Logs::OpenLogFile();
-    while (true) {
-
-	// Variables declaration, initialization and memory allocation
-	CurlWrapper * CurlVar = new CurlWrapper();
-	string strContent;
-	char *tmpContent = new char[100];
-	int FileId, TimeLimit, MemoryLimit, ixS = 0, ixE = 0;
-	char *content, *token, *ix, FileInfo[100];
-	char *ProblemId = new char[10];
-	char *lang = new char[10];
-
-	// Processing begins
-	if (CurlVar->FetchContentFromWebpage(&strContent) == -1) {
-	    delete CurlVar;		//Clean up
-	    Logs::WriteLine("Going to sleep for 5 seconds");
-	    sleep(5);
-	    continue;
-	}
-
-	content = new char[strContent.size() + 1];
-	sprintf(content, "%s", strContent.c_str());
-	Logs::WriteLine(content, true);	// Output fetched content on to the logs
-
-	char logs[100];
-	while (content[ixE] != '\0') {
-
-	    // Finding postion of the information for next file to be graded
-	    while (content[ixE] != '\n' && content[ixE] != '\0')
-			ixE++;
-	    
-	    if (content[ixE] == '\0')
-			break;
-	    
-	   	memcpy(FileInfo, content + ixS, ixE - ixS);
-	    FileInfo[ixE - ixS] = '\0';
-	    while (content[ixE] == '\n')
-		ixE++;
-	    ixS = ixE;
-
-	    // Extracting file specific information from the recieved content of the webpage
-	    token = strtok(FileInfo, " \n\r");
-	    FileId = atoi(token);
-	    token = strtok(NULL, " \n\r");
-	    sprintf(ProblemId, "%s", token);
-	    token = strtok(NULL, " \n\r");
-	    TimeLimit = atoi(token);
-	    token = strtok(NULL, " \n\r");
-	    MemoryLimit = atoi(token);
-	    token = strtok(NULL, " \n\r");
-	    sprintf(lang, "%s", token);
-	    printf("FileId: %d  ProblemId: %s  TimeLimit: %d  MemoryLimit: %d  lang: %s\n", FileId, ProblemId, TimeLimit, MemoryLimit, lang);
-
-	    // FileThread to carry out grading of the file
-	    //FileThread(FileId, ProblemId, TimeLimit, MemoryLimit, lang);
-	    FileHandle *F =	new FileHandle(FileId, ProblemId, TimeLimit, MemoryLimit, lang);
-	    F->Action();
-	    delete F;
-	}
-	delete CurlVar;		//Clean up
-	Logs::WriteLine("Going to sleep for 5 seconds", true);
-	sleep(5);
+	char cCurrentPath[FILENAME_MAX];
+	if (!getcwd(cCurrentPath, sizeof(cCurrentPath)))
+    {
+		return errno;
     }
-    Logs::CloseLogFile();
+
+	if(chdir(PATH)==-1)
+	{
+		printf("%d\n", errno);
+		printf("IE ERROR Cannot change directory to that of file");
+		return 1;
+	}
+	if (!getcwd(cCurrentPath, sizeof(cCurrentPath)))
+    {
+		return errno;
+    }
+
+	Logs::OpenLogFile();
+	Logs::CodeRunnerStarted();
+	Logs::CloseLogFile();
+	do{
+		Logs::OpenLogFile();
+		bool CurrentIteration = true;
+		ContentParser *ContentVar = new ContentParser();
+		if(ContentVar->FetchFileInfoList()!=0){
+			CurrentIteration = false;
+		}
+		
+		if(CurrentIteration && ContentVar->EndOfContent()){
+			Logs::WriteLine("File Queue Empty. Nothing to evaluate.");
+			CurrentIteration = false;
+		}
+		while(CurrentIteration && !ContentVar->EndOfContent()){
+			FileInfoStruct FileInfo = ContentVar->GetNextFileInfo();
+			FileHandle F(FileInfo.FileId, FileInfo.ProblemId, FileInfo.TimeLimit, FileInfo.MemoryLimit, FileInfo.lang);
+			F.Action();
+		} 
+		if(CurrentIteration) Logs::WriteLine("Current batch of files evaluated.");
+		
+		delete ContentVar;
+		Logs::GoToSleep();
+		Logs::CloseLogFile();
+		sleep(SLEEPINTERVAL);
+		
+	}while(!RUNONCE);
+		
     return 0;
 }
