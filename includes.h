bf090e92d4554012d77fc59d19077a011b53211e
Shashank Kumar <shashankkumar.me@gmail.com>
2011-11-23 18:01:44 +0530

    Added fcntl.h

--- includes.h
+++ includes.h
@@ -1,14 +1,18 @@
+#include <cctype>
+#include <cmath>
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cstdio>
-#include <cstdlib>
-#include <cmath>
-#include <cstring>
-#include <cctype>
-#include <ctime>
 
-#include <unistd.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <pwd.h>
+#include <signal.h>
 #include <sys/times.h>
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -16,8 +20,6 @@
 #include <sys/wait.h>
 #include <sys/errno.h>
 #include <sys/stat.h>
-#include <pwd.h>
-#include <signal.h>
-#include <limits.h>
+#include <unistd.h>
 
 using namespace std;
