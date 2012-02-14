d7b062b4ff0fc4bb3456567e855d9451c92bc6cf
Shashank Kumar <shashankkumar.me@gmail.com>
2012-02-14 23:34:41 +0530

    Removed includesh.h and includes.h and replaced with a single file headers.h to have one common header file.

---
+++ headers.h
@@ -0,0 +1,29 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+#include <cctype>
+#include <cmath>
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include <fcntl.h>
+#include <limits.h>
+#include <pwd.h>
+#include <signal.h>
+#include <sys/times.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/errno.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "Logs.h"
+#endif
