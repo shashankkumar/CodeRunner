2fc29ed4a530b5ab4c3c2418fbcb00fda9b4e33e
Shagun <sshagunsodhani@gmail.com>
2013-06-18 00:06:16 +0530

    Added date in link.sh to be logged into logs

---
+++ link.sh
@@ -0,0 +1,2 @@
+echo "CodeRunner Started on $(date)" >> /home/shagun/projects/CodeRunner/Logs/Logs2.txt 
+ /home/shagun/projects/CodeRunner/main -q >> /home/shagun/projects/CodeRunner/Logs/Logs2.txt
