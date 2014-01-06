f6a4256ac1e95f848b1f46bbdf6abec160b577a4
Shagun <sshagunsodhani@gmail.com>
2014-01-06 09:04:56 +0530

    modified sourcecode.php to add a memory intensive dummy program

--- Local/sourcecode.php
+++ Local/sourcecode.php
@@ -1,9 +1,23 @@
 <?php
 if($_REQUEST['username'] == 'shashank' && $_REQUEST['password'] = 'shashank'){
-echo "#include <iostream>
+/*echo "#include <iostream>
 using namespace std;
+void troll()
+{
+	int a[100];
+	troll();
+}
 main(){
 	int a=25;
 	cout<<\"7\"<<endl;
+	troll();
 }";
+*/
+echo "#include <iostream>
+using namespace std;
+main(){
+        int a=25;
+        cout<<\"7\"<<endl;
+}";
+
 }
